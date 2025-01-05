298e55f05a6ca0031cc2a6935640e740b33f01ea
Jason Cheng <ckaiwen3@hotmail.com>
2025-01-04 19:10:11 -0800

    Adding and modifying test artifacts

--- roboclipper_match_uploader/desktopTest.py
+++ roboclipper_match_uploader/desktopTest.py
@@ -3,6 +3,6 @@ import os
 
 from app import lambda_handler
 
-with open('../events/exampleSNS.json') as f:
+with open('../events/testPullWuM10.json') as f:
     testEvent = json.load(f)
 lambda_handler(testEvent, "")
